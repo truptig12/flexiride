f287b50ec79ce45079e0f7a5fa13cec33d61701f
trupti <tg22aaq@herts.ac.uk>
2023-09-18 22:44:00 +0100

    --disclaimer

---
+++ app/src/main/java/com/herts/flexiride/presentation/Disclaimer.kt
@@ -0,0 +1,12 @@
+package com.herts.flexiride.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.herts.flexiride.R
+
+class Disclaimer : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_disclaimer)
+    }
+}
